8bdda66ae6eb0ed36b8e3a8bbdf2cdf7cf708646
meletios <meletios@hedvig.com>
2018-11-21 18:16:07 +0100

    add toSting method for the enum, and new endpoint

--- constants/SanctionResult.go
+++ constants/SanctionResult.go
@@ -7,3 +7,15 @@ const (
 	PartialHit SanctionResult = 1
 	FullHit    SanctionResult = 2
 )
+
+func (s SanctionResult) ToString() string {
+	switch s {
+	case NoHit:
+		return "NoHit"
+	case PartialHit:
+		return "PartialHit"
+	case FullHit:
+		return "FullHit"
+	}
+	return ""
+}
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"meerkat/mappers"
 	"meerkat/queries"
@@ -12,11 +13,14 @@ import (
 	"github.com/gorilla/mux"
 )
 
-type SanctionResult uint16
-
 var mu sync.RWMutex
 var euList mappers.SanctionEntites
 var unList mappers.IndividualRoot
+
+type Response struct {
+	Query  string
+	Result string
+}
 
 func main() {
 	start_fetch := time.Now()
@@ -46,24 +50,38 @@ func main() {
 		}
 	}()
 
-	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/api/check", checkSanctionStatus).Methods(http.MethodGet).Queries("query", "{result}")
+	router := mux.NewRouter()
+	router.HandleFunc("/api/check", checkStatus).Methods(http.MethodGet).Queries("query", "{query}")
+	router.HandleFunc("/api/check", checkStatusWithFirstAndLastName).Methods(http.MethodGet).Queries("firstName", "{firstName}", "lastName", "{lastName}")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
-func checkSanctionStatus(w http.ResponseWriter, r *http.Request) {
-
+func checkStatus(w http.ResponseWriter, r *http.Request) {
 	start_sanct := time.Now()
 
 	vars := mux.Vars(r)
-	query := strings.Split(vars["result"], ",")
+	query := strings.Split(vars["query"], " ")
 
 	mu.Lock()
 	result := queries.QueryEUsanctionList(query, euList)
 	mu.Unlock()
 
-	w.WriteHeader(200)
-	w.Write([]byte("TEST"))
-	log.Println("Sanction Result ", result)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(Response{vars["query"], result.ToString()})
+	log.Println("Sanctionlist took ", time.Since(start_sanct))
+}
+
+func checkStatusWithFirstAndLastName(w http.ResponseWriter, r *http.Request) {
+	start_sanct := time.Now()
+
+	vars := mux.Vars(r)
+	query := []string{vars["firstName"], vars["lastName"]}
+
+	mu.Lock()
+	result := queries.QueryEUsanctionList(query, euList)
+	mu.Unlock()
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(Response{vars["firstName"] + " " + vars["lastName"], result.ToString()})
 	log.Println("Sanctionlist took ", time.Since(start_sanct))
 }
