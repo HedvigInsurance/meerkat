1e3e0dda33c22cc1010d017b80e0c5655527c689
meletios <meletios@hedvig.com>
2018-11-21 15:52:38 +0100

    initial tests

---
+++ main_test.go
@@ -0,0 +1,47 @@
+package main_test
+
+import (
+	"meerkat/constants"
+	"meerkat/mappers"
+	"meerkat/queries"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Main", func() {
+
+	var euList mappers.SanctionEntites
+
+	BeforeSuite(func() {
+		euList = mappers.MapEuSanctionList()
+	})
+
+	Describe("Testing Querying function", func() {
+		Context("query a non-terrosit", func() {
+			It("should be a No-HIT", func() {
+
+				query := []string{"Meletis", "Mazarakiotis"}
+
+				Expect(queries.QueryEUsanctionList(query, euList)).To(Equal(constants.NoHit))
+			})
+		})
+		Context("query a terrosit", func() {
+			It("should be a Parcial-HIT", func() {
+
+				query := []string{"Ricardo", "Ayeras"}
+
+				Expect(queries.QueryEUsanctionList(query, euList)).To(Equal(constants.FullHit))
+			})
+		})
+		Context("query a terrosit", func() {
+			It("should be a Full-HIT", func() {
+
+				query := []string{"Ricardo", "Mazarakiotis"}
+
+				Expect(queries.QueryEUsanctionList(query, euList)).To(Equal(constants.PartialHit))
+			})
+		})
+	})
+
+})
